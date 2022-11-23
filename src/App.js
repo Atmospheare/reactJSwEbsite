fff3b301e77c791e503fd907e7e19ae04dc48bdb
Michael Kurniawan <kurniawan.michael24@gmail.com>
2022-11-23 12:54:15 +0700

    seccond commit [23/11/2022]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // auto complete rafce
-import React from 'react'
+import React from "react";
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
 
 const App = () => {
   return (
     <div>
-        <h1>
-            GPT-3
-        </h1>
+      <h1>GPT-3</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
